3e811b25745508b7f0ba3d5fd9750f30034c3944
Eduu.pee <edumh2004@gmail.com>
2026-06-01 01:42:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+const inicio = document.getElementById("inicio");
+const menu = document.getElementById("menu");
+
+document.getElementById("entrar").addEventListener("click", () => {
+
+    inicio.style.display = "none";
+    menu.style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
+function ocultarPaginas(){
+
+    document.querySelectorAll(".pagina").forEach(pagina => {
+
+        pagina.style.display = "none";
+
+    });
+
+}
+
+function mostrarHistoria(){
+
+    ocultarPaginas();
+
+    menu.style.display = "none";
+
+    document.getElementById("historia").style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
+
+function mostrarRecuerdos(){
+
+    ocultarPaginas();
+
+    menu.style.display = "none";
+
+    document.getElementById("recuerdos").style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
+
+function mostrarCarta(){
+
+    ocultarPaginas();
+
+    menu.style.display = "none";
+
+    document.getElementById("carta").style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
+
+function mostrarRegalo(){
+
+    ocultarPaginas();
+
+    menu.style.display = "none";
+
+    document.getElementById("regalo").style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
+
+function volverMenu(){
+
+    ocultarPaginas();
+
+    menu.style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
